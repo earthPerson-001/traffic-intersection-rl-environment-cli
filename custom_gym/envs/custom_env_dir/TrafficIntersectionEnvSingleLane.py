a0f9f6a0bc9f15e023890b687216f237187b2265
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2022-08-11 18:55:27 +0545

    Changed the logic of single lane intersection

---
+++ custom_gym/envs/custom_env_dir/TrafficIntersectionEnvSingleLane.py
@@ -0,0 +1,268 @@
+from functools import reduce
+import gym
+import gym.spaces
+import numpy
+import random
+
+
+'''
+This models a type of intersection in which vechicle in a lane can move if corresponding 
+traffic light is green i.e. if light 1 is green then vehicle of lane 1 is free to move anywhere but 
+all other lanes is stopped. Such system will only have one lane for each direction (left and right)
+'''
+
+
+TOTAL_NUMBER_OF_LANES = 8
+NUMBER_OF_LANES_TO_OBSERVE = TOTAL_NUMBER_OF_LANES/2 # the lane number is in anticlockwise order skipping one lane
+LANES_CAPACITY = [100, 75, 80, 88]  # capacity of the corresponding lane
+PROBABILITY_OF_VEHICLE_OCCURANCE_IN_LANES_PER_SECOND = [0.2, 0.11, 0.29, 0.4]
+
+'''
+vehicles in those lanes will move with this speed (in km/hr ) if green light is on
+this is the average speed each vehicle will take when passing (assuming ideal case)
+'''
+VEHICLE_SPEED_OF_LANES = [14, 13, 15, 8]  
+
+MIN_GREEN_TIME = 30   # This corresponds to minimum green light time (in seconds) which is equivalent to one step in our environment
+
+''' 
+The possible configuration in 4 way, two lanes intersection is 4 
+
+            |     |      |
+            |     |      |
+            |     |      |
+            |     |      |
+            |     |      |
+------------              -------------
+ 
+------------              -------------
+
+------------              --------------
+            |     |      |
+            |     |      |
+            |     |      |
+            |     |      |
+            |     |      |
+
+
+The possible green configurations are:
+
+0)
+
+
+            |     |      |
+            |     |      |
+            |  ^  |      |
+            |  |  |      |
+            |     |      |
+------------              -------------
+     ->                         ->          
+------------              -------------     
+                                               
+------------              --------------    
+            |     |      |
+            |     |      |
+            |     |   |  |
+            |     |   V  |
+            |     |      |
+
+1)
+
+
+            |     |      |
+            |     |      |
+            |     |   |  |
+            |     |   V  |
+            |     |      |
+------------              -------------
+                                ->
+------------              -------------   
+       <-
+------------              --------------    
+            |     |      |                 
+            |     |      |
+            |     |      |
+            |     |   |  |
+            |     |   V  |
+
+
+2)
+
+            |     |      |
+            |     |      |
+            |  ^  |      |
+            |  |  |      |
+            |     |      |
+------------              ------------- 
+                                  
+------------               ------------ 
+    <-                        <-       
+------------               -------------
+            |     |      |
+            |     |   |  |
+            |     |   V  |
+            |     |      |
+            |     |      |
+
+3)
+
+            |     |      |
+            |     |      |
+            |  ^  |      |
+            |  |  |      |
+            |     |      |
+------------              ------------- 
+                             ->           
+------------              ------------- 
+        <-                                    
+------------              --------------
+            |     |      |
+            |     |      |
+            |  ^  |      |
+            |  |  |      |
+            |     |      |
+
+
+
+'''
+
+POSSIBLE_TRAFFIC_LIGHT_CONFIGURATION = 4
+
+LENGTH_STRAIGHT_IN_METERS = 15
+LENGTH_DIAGONAL_IN_METERS = LENGTH_STRAIGHT_IN_METERS * 1.41
+
+PERCENTAGE_OF_VEHICLE_GOING_THROUGH_SIDE_LANES = 0.3
+PERCENTAGE_OF_VEHICLE_GOING_DIAGONALLY = 0.2
+ONE_TRAINING_TIME = 5 * 60 # Train for the equivalent of 5 minutes
+
+class TrafficIntersectionEnvSingleLane(gym.Env):
+
+    def __init__(self):
+
+        # initialising gym stuffs
+        
+        '''
+        action space
+        The action which can be performed is changing the current active traffic light configuration
+        '''
+        self.action_space = gym.spaces.Discrete(POSSIBLE_TRAFFIC_LIGHT_CONFIGURATION)
+
+        # observation space
+        # Observation space is the vehicle count in different lanes which need to be observed 
+        self.observation_space = gym.spaces.Box(low=0, high=1000, shape=(int(NUMBER_OF_LANES_TO_OBSERVE), ), dtype=numpy.float64)
+
+        # keeping track of the reward
+        # self.collected_reward = -1
+
+
+        # initialising our environment
+
+        # each element of array corresponds to vehicle count in the corresponding lane
+        self.lanes = numpy.zeros(int(NUMBER_OF_LANES_TO_OBSERVE))
+
+        # randomising vehicle count in lanes
+        for i in range(int(NUMBER_OF_LANES_TO_OBSERVE)):
+            self.lanes[i] = random.randint(int(0.1 * LANES_CAPACITY[i]), LANES_CAPACITY[i])
+
+        self.state = self.lanes
+
+        self.percentageOfVehiclePassingThroughTheIntersectionLastTime = 0
+
+
+        self.remaining_time = ONE_TRAINING_TIME
+
+        
+    def step(self, action):
+        '''
+        The possible action is to change the traffic light configuration.
+        If configuration is set to the same one, yellow light shouldn't turn on. 
+        '''
+
+        done = False
+        info = {}
+
+        self.remaining_time -= MIN_GREEN_TIME
+
+        self.state = action
+
+        vehicleThroughIntersection, vehicleRemaining = self.simulateTraffic()
+
+        if vehicleThroughIntersection==0 or self.remaining_time < MIN_GREEN_TIME:
+            done = True
+        
+        reward = self.calculateReward(vehicleThroughIntersection, vehicleRemaining)
+        #self.collected_reward += reward
+
+        obs = self.lanes
+
+        if done==True:
+            self.reset()
+
+        return obs, reward, done, info
+
+
+    def reset(self):
+
+        # randomising vehicle count in lanes
+        for i in range(int(NUMBER_OF_LANES_TO_OBSERVE)):
+            self.lanes[i] = random.randint(int(0.1 * LANES_CAPACITY[i]), LANES_CAPACITY[i])
+
+        #self.collected_reward = -1
+        self.state = self.lanes
+
+        self.percentageOfVehiclePassingThroughTheIntersectionLastTime = 0
+
+        self.remaining_time = ONE_TRAINING_TIME
+
+        return self.state
+
+    def simulateTraffic(self):
+
+        trafficLightConfiguration = self.state
+        vehicleThroughIntersection = 0
+        totalVehicle = reduce(lambda x,y: x+y, self.lanes)
+
+        # reward calculation based on number of vehicle passed
+        
+        # since vehicles can move randomly (in the real world), we can set any type of logic here, but keeping it a bit realistic
+
+        vehiclesTurning = PERCENTAGE_OF_VEHICLE_GOING_THROUGH_SIDE_LANES * self.lanes[trafficLightConfiguration]
+        vehiclesTurningDiagonally = PERCENTAGE_OF_VEHICLE_GOING_DIAGONALLY * self.lanes[trafficLightConfiguration]
+        vehiclesGoingStraight = self.lanes[trafficLightConfiguration] - vehiclesTurning - vehiclesTurningDiagonally
+
+        timeTakenForTurning = 0.1 * LENGTH_STRAIGHT_IN_METERS / (0.7 * VEHICLE_SPEED_OF_LANES[trafficLightConfiguration] * (10/36))
+        timeTakenForDiagonalTurning = LENGTH_DIAGONAL_IN_METERS / (VEHICLE_SPEED_OF_LANES[trafficLightConfiguration]* (10/36))
+        timeTakenStraight = LENGTH_STRAIGHT_IN_METERS / (VEHICLE_SPEED_OF_LANES[trafficLightConfiguration]* (10/36))
+
+        maxVehicleTurning = MIN_GREEN_TIME / timeTakenForTurning
+        maxVehicleTurningDiagonally = MIN_GREEN_TIME / timeTakenForDiagonalTurning
+        maxVehicleStraight = MIN_GREEN_TIME / timeTakenStraight
+
+        actualVehicleTurning = min(maxVehicleTurning, vehiclesTurning)
+        actualVehicleTurningDiagonally = min(maxVehicleTurningDiagonally, vehiclesTurningDiagonally)
+        actualVehicleGoingStraight = min(maxVehicleStraight, vehiclesGoingStraight)
+        
+        vehicleThroughIntersection = int(actualVehicleTurning + actualVehicleTurningDiagonally + actualVehicleGoingStraight)
+
+        self.lanes[trafficLightConfiguration] -= vehicleThroughIntersection
+        vehicleRemaining = totalVehicle - vehicleThroughIntersection
+        
+        # adding vehicles to the lanes according to the probability
+        for j,probability in enumerate(PROBABILITY_OF_VEHICLE_OCCURANCE_IN_LANES_PER_SECOND):
+            self.lanes[j] += int(probability * MIN_GREEN_TIME)
+
+
+        return vehicleThroughIntersection, vehicleRemaining
+
+    # dummy reward function for now
+    # this doesn't work
+    def calculateReward(self, numberOfVehiclePassed, numberOfVehicleRemaining):
+
+        # previous observation to see what was the best possible choice
+        previousObservation = self.lanes.copy()
+        for j,probability in enumerate(PROBABILITY_OF_VEHICLE_OCCURANCE_IN_LANES_PER_SECOND):
+            previousObservation[j] -= int(probability * MIN_GREEN_TIME)
+
+        reward = numberOfVehiclePassed / max(previousObservation)
+
+        return reward
